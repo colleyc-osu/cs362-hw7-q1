5cd75567e9cf50af5f643fbd7b57f6df1ca3454a
Casey Colley <colleyc@oregonstate.edu>
2021-03-06 14:47:16 -0800

    writes FizzBuzz on %15; tabs were wrong in test 5

--- question1.py
+++ question1.py
@@ -1,7 +1,9 @@
 def fizzbuzz(r):
 	result = ''
 	for x in range(1, r+1):
-		if (x % 3 == 0):
+		if (x % 15 == 0):
+			result += "FizzBuzz"
+		elif (x % 3 == 0):
 			result += "Fizz"
 		elif (x % 5 == 0):
 			result += "Buzz"
