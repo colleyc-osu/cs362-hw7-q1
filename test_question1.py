a40f2351a7217ca8597c155f2ca7c4b1d4a60054
Casey Colley <colleyc@oregonstate.edu>
2021-03-06 14:31:49 -0800

    tc2, should write fizz as appropriate

--- test_question1.py
+++ test_question1.py
@@ -6,6 +6,9 @@ class TestCase(unittest.TestCase):
 	def test1(self):
 		self.assertEqual(question1.fizzbuzz(2), "1	2")
 
+	def test2(self):
+		self.assertEqual(question1.fizzbuzz(4), "1	2	Fizz	4")
+
 
 if __name__ == '__main__':
 	unittest.main()
